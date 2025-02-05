f9f2528caa0a022b2126b10d00b6592f954adda7
Goldbach <goldbacm@oregonstate.edu>
2025-02-05 12:07:37 -0800

    version2.5.25

--- js/lab3.js
+++ js/lab3.js
@@ -1,38 +1,64 @@
-var map = L.map('pointmap').setView([44.7, -122.5], 4);
+//declare map variable globally so all functions have access
+var map;
+var minValue;
 
+//creating map for the proportional symbols    
+var map = L.map('coolmap').setView([44.7, -122.5], 4);
+
+//adding openstreet map tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-fetch('/data/airports.geojson')
+//get the data
+fetch('/data/USA_Major_Cities.geojson')
     .then(response => response.json())
     .then(data => {
     L.geoJSON(data).addTo(map);
     })
-    .catch(error => console.error('Error: ', error));
-
-
-    
-var map2 = L.map('coolmap').setView([44.7, -122.5], 4);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map2);
-
-fetch('/data/USA_Major_Cities.geojson')
-    .then(response => response.json())
-    .then(data => {
-    L.geoJSON(data).addTo(map2);
+    .then(function(json){
+        //call function to create proportional symbols
+        createPropSymbols(json);
     })
     .catch(error => console.error('Error: ', error));
+    
 
-//var map = L.map('coolmap').setview([44.56, -123.26], 12); //this zooms to corvallis
-//created a VARIABLE called map, send to div created, and go to these coordinates 
-//creates a map object but need to load tile set to see the map
-//L.tilelayer does this
-//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map); //addTo is how you add data to your map object 
+// create proportional symbol
+//Add circle markers for point features to the map
+function createPropSymbols(data){
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data){
+//Determine the attribute for scaling the proportional symbols
+var attribute = "Pop_2015";
+L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        //Step 5: For each feature, determine its value for the selected attribute
+        var attValue = Number(feature.properties[attribute]);
+
+        //examine the attribute value to check that it is correct
+        console.log(feature.properties, attValue);
+
+        //create circle markers
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
+};
